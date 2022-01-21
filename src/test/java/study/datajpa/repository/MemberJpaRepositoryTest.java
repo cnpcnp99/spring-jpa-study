d16906eedf2baec4d4ac1bc16e10cbe507204bbb
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2022-01-21 20:59:58 +0900

    일반 JPA Repository와 SpringJPA Repository의 기능 확인

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,41 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    private MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+
+        // when
+        Member foundMember = memberJpaRepository.find(savedMember.getId());
+
+        // then
+        assertThat(foundMember.getId()).isEqualTo(member.getId());
+        assertThat(foundMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(foundMember).isEqualTo(member);
+
+    }
+
+
+
+}
---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+
+        // when
+        Member foundMember = memberRepository.findById(savedMember.getId()).get();
+
+        // then
+        assertThat(foundMember.getId()).isEqualTo(member.getId());
+        assertThat(foundMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(foundMember).isEqualTo(member);
+
+    }
+
+}
