d50aa4f2cc0e3fed7fbc1720d65dd02eaad92e9e
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2022-01-21 21:09:20 +0900

    Member, Team 엔티티 확장

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -5,10 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,8 +16,37 @@ public class Member {
 
     private String username;
 
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+
     public Member(String username) {
         this.username = username;
+        this.age = 0;
     }
 
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+        this.team = team;
+    }
+
+    /**
+     * 연관관계 한 번에 처리
+     */
+    private void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
